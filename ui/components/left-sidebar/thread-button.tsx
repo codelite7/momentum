899d785f0acab14d224835c9fb83a4cb3660f1a3
Zack Stevens <zack.st7@gmail.com>
2024-05-26 18:10:26 -0600

    in progress commit

--- ui/components/left-sidebar/thread-button.tsx
+++ ui/components/left-sidebar/thread-button.tsx
@@ -7,7 +7,6 @@ interface ThreadButtonProps {
 }
 
 export default function ThreadButton({edge}: ThreadButtonProps) {
-    const [showButton, setShowButton] = useState(false);
     const [popoverOpen, setPopoverOpen] = useState(false);
     const [hovered, setHovered] = useState(false);
     const router = useRouter()
@@ -16,80 +15,64 @@ export default function ThreadButton({edge}: ThreadButtonProps) {
             {
                 hovered || popoverOpen ? (
                     <>
-                        {/*<Button*/}
-                        {/*    size="sm"*/}
-                        {/*    className="flex flex-row start rounded-r-none w-full"*/}
-                        {/*    onClick={() => router.push(`/thread/${edge?.node?.id}`)}*/}
-                        {/*>*/}
-                        {/*    <div className="w-full flex flex-row start">*/}
-                        {/*        {edge?.node?.name}*/}
-                        {/*    </div>*/}
-                        {/*</Button>*/}
-                    <Popover
-                        placement="bottom-start"
-                        classNames={{
-                            content: [
-                                "rounded border border-default-200 p-0 m-0"
-                            ]
-                        }}
-                        onOpenChange={(isOpen) => {
-                            setPopoverOpen(isOpen)
-                        }}
-                    >
-                        <PopoverTrigger>
-                            <Button
-                                size="sm"
-                                className="flex flex-row start w-full"
-                                onClick={() => setPopoverOpen(true)}
-                            >
-                                <div className="flex start items-center justify-between w-full">
-                                    <div>
-                                        {edge?.node?.name}
-                                    </div>
-                                    <i className="pi pi-ellipsis-v"></i>
+                        <Button
+                            size="sm"
+                            className="flex flex-row start w-full"
+                            onClick={() => router.push(`/thread/${edge?.node?.id}`)}
+                        >
+                            <div className="flex start items-center justify-between w-full">
+                                <div>
+                                    {edge?.node?.name}
                                 </div>
-                            </Button>
-                            {/*<Button*/}
-                            {/*    isIconOnly*/}
-                            {/*    size="sm"*/}
-                            {/*    className="rounded-l-none"*/}
-                            {/*    onClick={() => setPopoverOpen(true)}*/}
-                            {/*>*/}
-                            {/*    <i className="pi pi-ellipsis-v"></i>*/}
-                            {/*</Button>*/}
-                        </PopoverTrigger>
-                        <PopoverContent>
-                            <div className="flex flex-col">
-                                <div
-                                    className="flex flex-row items-center hover:bg-default-200 cursor-pointer pl-4 pr-4 pt-2 pb-2">
-                                    <div className="mr-4">
-                                        <img src="/edit.svg" alt="edit thread name"/>
-                                    </div>
-                                    Rename
-                                </div>
-                                <div
-                                    className="flex flex-row items-center hover:bg-default-200 cursor-pointer pl-4 pr-4 pt-2 pb-2"
-                                    onClick={() => {
-                                        console.log(`deleting thread: ${edge?.node?.id}`)
-                                    }}>
-                                    <div className="mr-4">
-                                        <img src="/trash.svg" alt="delete thread"/>
-                                    </div>
-                                    Delete
-                                </div>
+                                <Popover
+                                    placement="bottom-start"
+                                    classNames={{
+                                        content: [
+                                            "rounded border border-default-200 p-0 m-0"
+                                        ]
+                                    }}
+                                    onOpenChange={(isOpen) => {
+                                        setPopoverOpen(isOpen)
+                                    }}
+                                >
+                                    <PopoverTrigger>
+                                        <i className="pi pi-ellipsis-v"></i>
+                                    </PopoverTrigger>
+                                    <PopoverContent>
+                                    <div className="flex flex-col">
+                                            <div
+                                                className="flex flex-row items-center hover:bg-default-200 cursor-pointer pl-4 pr-4 pt-2 pb-2">
+                                                <div className="mr-4">
+                                                    <img src="/edit.svg" alt="edit thread name"/>
+                                                </div>
+                                                Rename
+                                            </div>
+                                            <div
+                                                className="flex flex-row items-center hover:bg-default-200 cursor-pointer pl-4 pr-4 pt-2 pb-2"
+                                                onClick={() => {
+                                                    console.log(`deleting thread: ${edge?.node?.id}`)
+                                                }}>
+                                                <div className="mr-4">
+                                                    <img src="/trash.svg" alt="delete thread"/>
+                                                </div>
+                                                Delete
+                                            </div>
+                                        </div>
+                                    </PopoverContent>
+                                </Popover>
                             </div>
-                        </PopoverContent>
-                    </Popover>
+                        </Button>
                     </>
                 ) : (
                     <Button
                         size="sm"
-                        fullWidth
-                        className="flex flex-row start rounded-r-none w-full bg-transparent"
+                        className="flex flex-row start w-full bg-transparent"
                         onClick={() => router.push(`/thread/${edge?.node?.id}`)}
                     >
-                        <div className="w-full flex flex-row start">
-                            {edge?.node?.name}
+                        <div className="flex start items-center justify-between w-full">
+                            <div>
+                                {edge?.node?.name}
+                            </div>
                         </div>
                     </Button>
                 )
